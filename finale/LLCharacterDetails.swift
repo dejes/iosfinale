f29236c799188696c9becc7c5fd97f6bb81aa19c
TsaiHanChing <moon5050808@gmail.com>
2019-06-14 20:01:23 +0800

    idol completed without dynamic table

--- finale/LLCharacterDetails.swift
+++ finale/LLCharacterDetails.swift
@@ -9,13 +9,20 @@
 import Foundation
 
 struct RoleDetail:Codable{
+    var chibi:URL
     var name:String
     var japanese_name:String
     var age:Int
     var birthday:String
     var main_unit:String
     var sub_unit:String
-    var
+    var height:Int
+    var year : String
+    var cv:cv
+    
+}
+struct cv:Codable {
+    var name:String
 }
   var musecharacter = ["Hoshizora Rin","Nishikino Maki","Koizumi Hanayo","Kousaka Honoka","Sonoda Umi","Minami Kotori"," Ayase Eli","Yazawa Nico","Toujou Nozomi"]
 var musecharacterapi = ["Hoshizora%20Rin","Nishikino%20Maki","Koizumi%20Hanayo","Kousaka%20Honoka","Sonoda%20Umi","Minami%20Kotori","Ayase%20Eli","Yazawa%20Nico","Toujou%20Nozomi"]
--- finale/LLRoleDetailsTableViewController.swift
+++ finale/LLRoleDetailsTableViewController.swift
@@ -18,7 +18,9 @@ class LLRoleDetailsTableViewController: UITableViewController {
     @IBOutlet weak var unit_label: UILabel!
     @IBOutlet weak var subunit_label: UILabel!
     @IBOutlet weak var height_label: UILabel!
-    @IBOutlet weak var seiyu_label: UILabel!
+    @IBOutlet weak var cv_label: UILabel!
+    @IBOutlet weak var year_label: UILabel!
+    @IBOutlet weak var Chibi_img: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         let hyperlink="http://schoolido.lu/api/idols/"
@@ -28,16 +30,22 @@ class LLRoleDetailsTableViewController: UITableViewController {
                 (data,response,error) in
                 let decoder=JSONDecoder()
                 if let data=data,let roleDetail=try? decoder.decode(RoleDetail.self, from: data){
-                    
-                    DispatchQueue.main.async {
-                        self.jp_name_label.text = roleDetail.japanese_name
-                        self.name_label.text="Name:"+roleDetail.name
-                        self.age_label.text="Age:"+String(roleDetail.age)
-                        self.birthday_label.text="Birthday:"+roleDetail.birthday
-                        self.unit_label.text="Unit:"+roleDetail.main_unit
-                        self.subunit_label.text="Sub Unit:"+roleDetail.sub_unit
-                        self.tableView.reloadData()
+                    if let imgdata=try? Data (contentsOf: roleDetail.chibi){
+                        DispatchQueue.main.async {
+                            self.Chibi_img.image=UIImage(data: imgdata)
+                            self.jp_name_label.text = roleDetail.japanese_name
+                            self.name_label.text="Name: "+roleDetail.name
+                            self.age_label.text="Age: "+String(roleDetail.age)
+                            self.birthday_label.text="Birthday: "+roleDetail.birthday
+                            self.unit_label.text="Unit: "+roleDetail.main_unit
+                            self.subunit_label.text="Sub Unit: "+roleDetail.sub_unit
+                            self.height_label.text="Height: "+String(roleDetail.height)+" cm"
+                            self.cv_label.text="CV: "+roleDetail.cv.name
+                            self.year_label.text="Grade:"+roleDetail.year+" grade"
+                            self.tableView.reloadData()
+                        }
                     }
+                    
                 }
                 else{
                     print("errorin")
